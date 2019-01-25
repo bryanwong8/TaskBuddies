08ef6027da44a79fffa9a8eb0e0a34a7d14035a6
Bryan Wong <bryanwong7788@gmail.com>
2019-01-25 11:42:12 -0800

    Initialized User model with devise and set up routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
+  
+  get 'pages/index'
+  root to: 'pages#index'
 end
